562d04337fade8b78ca21be38489f656bb7e1bee
subhasishnath45 <subhasishnath45@gmail.com>
2023-03-04 13:41:25 +0530

    AOS class demo on 4th March 2023.

---
+++ main.js
@@ -0,0 +1,11 @@
+// https://github.com/michalsnik/aos
+// https://michalsnik.github.io/aos/
+// https://codepen.io/subhasishnath45/pen/poOwzMa?editors=0010
+
+$(document).ready(function(){
+    AOS.init({
+        delay: 10, // values from 0 to 3000, with step 50ms
+        duration: 400, // values from 0 to 3000, with step 50ms
+    });
+})
+
